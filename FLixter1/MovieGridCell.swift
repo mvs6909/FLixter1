af2f254911af5820c7d45b15139ae8f4f015df40
Manav Shah <mvs6909@psu.edu>
2021-02-27 17:35:00 -0500

    Flixter 1

---
+++ FLixter1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FLixter1
+//
+//  Created by Manav Shah on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
